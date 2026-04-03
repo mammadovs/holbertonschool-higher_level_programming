caace84ea183072f4ebd49c7be3651aabe61b746
mammadovs <12747@holbertonstudents.com>
2026-04-03 14:01:27 +0000

    0. My first square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
-s = Square()
-print(s)  # <__main__.Square object at 0x...>
+"""
+Module 0-square
+
+Defines a class Square.
+The class is currently empty and serves as a template for future square implementations.
+"""
+
+class Square:
+    """
+    Class Square represents a square.
+
+    Currently, the class has no attributes or methods.
+    It is intended as a base structure for creating square objects.
+    """
+    pass
