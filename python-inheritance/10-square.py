a7b3bb73726adfcb443020e877e369bfa4ffa44f
mammadovs <12747@holbertonstudents.com>
2026-04-12 10:05:55 +0000

    10. Square #1

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -15,13 +15,8 @@ class Square(Rectangle):
         Args:
             size (int): The dimension of the square's sides
         """
-        # Validate size using the inherited integer_validator
         self.integer_validator("size", size)
-        
-        # Initialize the parent Rectangle with size as both width and height
         super().__init__(size, size)
-        
-        # Assign to private attribute
         self.__size = size
 
     def area(self):
