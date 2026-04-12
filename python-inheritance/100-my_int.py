546ab3b7859b21ca2cd0f8f0d7226e1e756d07f7
mammadovs <12747@holbertonstudents.com>
2026-04-12 10:40:55 +0000

    12. My integer

--- python-inheritance/100-my_int.py
+++ python-inheritance/100-my_int.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 """
-This module defines the MyInt class, which is a rebellious 
-integer that inverts equality operators.
+This module defines the MyInt class.
 """
+
 
 class MyInt(int):
     """
     MyInt is a rebel. MyInt has == and != operators inverted.
     """
-    
+
     def __eq__(self, other):
-        """Override == with != behavior"""
+        """Override == with != behavior."""
         return super().__ne__(other)
 
     def __ne__(self, other):
-        """Override != with == behavior"""
+        """Override != with == behavior."""
         return super().__eq__(other)
