254a4f3484e0b72ce5948c8edc84bbb5963ce7b2
mammadovs <12747@holbertonstudents.com>
2026-04-19 09:00:11 +0000

    7. Load, add, save

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
 """
 Script that adds all arguments to a Python list and saves them to a file.
 """
 import sys
+
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
