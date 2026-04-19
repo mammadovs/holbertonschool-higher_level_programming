fbf87d191d1aec9c93d471d2d972e762f3b057a2
mammadovs <12747@holbertonstudents.com>
2026-04-19 08:55:36 +0000

    4. From JSON string to Object

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -6,5 +6,5 @@ import json
 
 
 def from_json_string(my_str):
-    """Returns an object (Python data structure) represented by a JSON string."""
+    """Returns a Python object represented by a JSON string."""
     return json.loads(my_str)
