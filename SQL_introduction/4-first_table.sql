2bf1bba9c1237a0b4f7cc3283761afcfc76e7479
mammadovs <12747@holbertonstudents.com>
2026-04-30 08:50:08 +0000

    4. First table

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- Creates a table called first_table in the current database
+-- If the table already exists, the script should not fail
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
