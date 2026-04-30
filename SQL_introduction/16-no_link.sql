c1d4039a017b4a58a37f8c0bf9efab2e548b09f8
mammadovs <12747@holbertonstudents.com>
2026-04-30 09:03:35 +0000

    16. Say my name

---
+++ SQL_introduction/16-no_link.sql
@@ -0,0 +1,3 @@
+-- Lists all records of the table second_table
+-- Filtered to exclude rows without a name, ordered by score descending
+SELECT score, name FROM second_table WHERE name IS NOT NULL AND name <> '' ORDER BY score DESC;
