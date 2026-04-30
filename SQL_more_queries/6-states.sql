514f54cddba4e77c1ef61dfb79e8b46cee412a51
mammadovs <12747@holbertonstudents.com>
2026-04-30 09:24:00 +0000

    6. States table

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- Creates the database hbtn_0d_usa and the table states
+-- The table includes an auto-generated primary key and a non-null name
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+    id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
