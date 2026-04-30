5d82fbbb391c91da56af4ca1d0e6b98bb3823e09
mammadovs <12747@holbertonstudents.com>
2026-04-30 09:38:19 +0000

    18. No Comedy tonight!

---
+++ SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,12 @@
+-- Lists all shows without the genre Comedy in the database hbtn_0d_tvshows
+-- Uses a subquery to find shows linked to Comedy and excludes them
+SELECT title
+FROM tv_shows
+WHERE title NOT IN (
+    SELECT tv_shows.title
+    FROM tv_shows
+    JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+    JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+    WHERE tv_genres.name = 'Comedy'
+)
+ORDER BY title ASC;
