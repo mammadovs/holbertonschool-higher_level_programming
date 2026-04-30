e0aaefafdc2acf0f49126a9b0d594d0725501211
mammadovs <12747@holbertonstudents.com>
2026-04-30 09:17:49 +0000

    0. My privileges!

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- Lists all privileges of the MySQL users user_0d_1 and user_0d_2
+-- This script displays the grants for specific users on localhost
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
